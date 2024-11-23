9789ba89089a193acd4966fbdc7a5db7c49ebb27
Hckerson <166761968+Hckerson@users.noreply.github.com>
2024-11-23 16:59:11 +0100

    complete sales page

--- app/ui/components/FAQ.tsx
+++ app/ui/components/FAQ.tsx
@@ -974,41 +974,397 @@ export function ComplexFaq() {
                     <span className="text-lg font-semibold  underline underline-offset-2 decoration-1  hover:no-underline ">
                       <Link href="">Salesforce</Link>
                     </span>
-                    
-                    —collaborate with your customers and team members on any of
-                    your content stored in Dropbox, directly from your
-                    Salesforce records
+                    <p className="font-light inline">
+                      —collaborate with your customers and team members on any
+                      of your content stored in Dropbox, directly from your
+                      Salesforce records
+                    </p>
                   </li>
                   <li>
                     <span className="text-lg font-semibold  underline underline-offset-2 decoration-1   hover:no-underline">
                       <Link href="">Zoho CRM</Link>
                     </span>
-                    —give sales reps a quick and easy solution to manage, create,
-                    share and access files in your Dropbox account from within
-                    Zoho CRM.
+                    <p className="font-light inline">
+                      —give sales reps a quick and easy solution to manage,
+                      create, share and access files in your Dropbox account
+                      from within Zoho CRM.
+                    </p>
                   </li>
                   <li>
                     <span className="text-lg font-semibold  underline underline-offset-2 decoration-1  hover:no-underline ">
                       <Link href="">Hubspot</Link>
                     </span>
-                    —access any Dropbox file from within your Hubspot workflow,
-                    sign documents seamlessly, and simplify processes to get
-                    more done—with less work
+                    <p className="font-light inline">
+                      —access any Dropbox file from within your Hubspot
+                      workflow, sign documents seamlessly, and simplify
+                      processes to get more done—with less work
+                    </p>
                   </li>
                   <li>
                     <span className="text-lg font-semibold  underline underline-offset-2 decoration-1   hover:no-underline">
                       <Link href="">amoCRM</Link>
                     </span>
-                    —attach Dropbox files directly to your lead card notes, and
-                    access Dropbox files directly from amoCRM (also known as
-                    Kommo CRM)
+                    <p className="font-light inline">
+                      —attach Dropbox files directly to your lead card notes,
+                      and access Dropbox files directly from amoCRM (also known
+                      as Kommo CRM)
+                    </p>
                   </li>
                   <li>
                     <span className="text-lg font-semibold  underline underline-offset-2 decoration-1   hover:no-underline">
                       <Link href="">Insightly</Link>
                     </span>
-                    —link files stored in your Dropbox account to your Insightly
-                    records, no need to upload to two separate platforms
+                    <p className="font-light inline">
+                      —link files stored in your Dropbox account to your
+                      Insightly records, no need to upload to two separate
+                      platforms
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="flex flex-col relative border-b border-black hover:bg-[#eee9e2] border-opacity-40 items-start box-border">
+              <div
+                className="cursor-pointer w-full box-border px-6 py-8 flex  justify-between space-x-2 items-center"
+                onClick={() => toggleMenu(1)}
+              >
+                <span
+                  className={clsx(
+                    "text-base font-semibold peer transition-colors ease-in-out"
+                  )}
+                >
+                  What do I get with a Dropbox plan?
+                </span>
+                <div className="p-3">
+                  {openMenu === 1 ? (
+                    <ChevronUpIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  ) : (
+                    <ChevronDownIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  )}
+                </div>
+              </div>
+              <div
+                className={clsx(
+                  "overflow-hidden transition-all duration-300 ease-in-out -translate-y-7",
+                  openMenu === 1
+                    ? "max-h-[500px] opacity-100"
+                    : "max-h-0 opacity-0"
+                )}
+              >
+                <ul
+                  style={{ scrollbarWidth: "none" }}
+                  className="w-full box-border px-6 flex flex-col gap-y-6 overflow-scroll"
+                >
+                  <li className="w-full text-base font-light">
+                    Our plans for individual users and teams give you access to
+                    a wide range of tools and features that help to speed up
+                    your sales workflow.
+                  </li>
+                  <li className="w-full text-base font-light">
+                    <span className="underline underline-offset-2 decoration-1 hover:decoration-white transition-colors duration-200 decoration-stone-400">
+                      Explore the latest plans and pricing
+                    </span>
+                    for details.
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="flex flex-col relative border-b border-black hover:bg-[#eee9e2] border-opacity-40 items-start box-border">
+              <div
+                className="cursor-pointer w-full box-border px-6 py-8 flex  justify-between space-x-2 items-center"
+                onClick={() => toggleMenu(2)}
+              >
+                <span
+                  className={clsx(
+                    "text-base font-semibold peer transition-colors ease-in-out"
+                  )}
+                >
+                  What tools does Dropbox offer for sales teams?
+                </span>
+                <div className="p-3">
+                  {openMenu === 2 ? (
+                    <ChevronUpIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  ) : (
+                    <ChevronDownIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  )}
+                </div>
+              </div>
+              <div
+                className={clsx(
+                  "overflow-hidden transition-all duration-300 ease-in-out -translate-y-7",
+                  openMenu === 2
+                    ? "max-h-[500px] opacity-100"
+                    : "max-h-0 opacity-0"
+                )}
+              >
+                <div className="px-6  lg:w-9/12 w-full mb-6">
+                  In addition to lightning-fast file syncing and collaborative
+                  capabilities, Dropbox provides a number of tools that make
+                  life easier for sales teams to engage leads and close deals:
+                </div>
+                <ul
+                  style={{ scrollbarWidth: "none" }}
+                  className="lg:w-4/5 w-full box-border px-6 flex flex-col gap-y-4 overflow-scroll  list-disc   pl-12"
+                >
+                  <li>
+                    <span className="text-lg font-semibold  underline underline-offset-2 decoration-1  hover:no-underline ">
+                      <Link href="/nav/products/sign">Sign</Link>
+                    </span>
+                    <p className="font-light inline">
+                      —all of your contracting and eSignature needs covered
+                    </p>
+                  </li>
+                  <li>
+                    <span className="text-lg font-semibold  underline underline-offset-2 decoration-1   hover:no-underline">
+                      <Link href="/nav/products/docsend">DocSend</Link>
+                    </span>
+                    <p className="font-light inline">
+                      —finalize, send, and track engagement with sales proposals
+                      and pitches
+                    </p>
+                  </li>
+                  <li>
+                    <span className="text-lg font-semibold  underline underline-offset-2 decoration-1  hover:no-underline ">
+                      <Link href="">Capture</Link>
+                    </span>
+                    <p className="font-light inline">
+                      —communicate with clarity, using short screen recordings
+                      to save writing lengthy emails
+                    </p>
+                  </li>
+                  <li>
+                    <span className="text-lg font-semibold  underline underline-offset-2 decoration-1   hover:no-underline">
+                      <Link href="">Transfer</Link>
+                    </span>
+                    <p className="font-light inline">
+                      —securely share large files, with password protection and
+                      custom branding
+                    </p>
+                  </li>
+                  <li>
+                    <span className="text-lg font-semibold  underline underline-offset-2 decoration-1   hover:no-underline">
+                      <Link href="">Passwords</Link>
+                    </span>
+                    <p className="font-light inline">
+                      —store passwords and log into platforms in a single click
+                    </p>
+                  </li>
+                  <li>
+                    <span className="text-lg font-semibold  underline underline-offset-2 decoration-1   hover:no-underline">
+                      <Link href="">Backup</Link>
+                    </span>
+                    <p className="font-light inline">
+                      —automatically back up important files and easily restore
+                      them to any device
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="flex flex-col  relative border-b border-black hover:bg-[#eee9e2] border-opacity-40 items-start box-border">
+              <div
+                className="cursor-pointer w-full box-border px-6 py-8 flex  justify-between space-x-2 items-center"
+                onClick={() => toggleMenu(3)}
+              >
+                <span
+                  className={clsx(
+                    "text-base font-semibold peer transition-colors ease-in-out"
+                  )}
+                >
+                  What sales team processes does Dropbox help with?
+                </span>
+                <div className="p-3">
+                  {openMenu === 3 ? (
+                    <ChevronUpIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  ) : (
+                    <ChevronDownIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  )}
+                </div>
+              </div>
+              <div
+                className={clsx(
+                  "overflow-hidden w-full transition-all duration-300 ease-in-out -translate-y-7",
+                  openMenu === 3
+                    ? "max-h-[500px] opacity-100"
+                    : "max-h-0 opacity-0"
+                )}
+              >
+                <div className="px-6  lg:w-9/12 w-full mb-6">
+                  Dropbox makes your life easier at every stage of the sales
+                  pipeline:
+                </div>
+                <ul
+                  style={{ scrollbarWidth: "none" }}
+                  className="lg:w-4/5 w-full box-border px-6 flex flex-col gap-y-4 overflow-scroll  list-disc   pl-12"
+                >
+                  <li>
+                    <p className="font-light inline">
+                      Create, store, and organize your pitch decks in your
+                      Dropbox cloud storage
+                    </p>
+                  </li>
+                  <li>
+                    <p className="font-light inline">
+                      Send pitches and proposals with DocSend
+                    </p>
+                  </li>
+                  <li>
+                    <p className="font-light inline">
+                      dd context to your proposals with screen recordings from
+                      Capture
+                    </p>
+                  </li>
+                  <li>
+                    <p className="font-light inline">
+                      When it’s time to sign new contracts or renewals, request
+                      eSignatures with Sign
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="flex flex-col relative border-b border-black hover:bg-[#eee9e2] border-opacity-40 items-start box-border">
+              <div
+                className="cursor-pointer w-full box-border px-6 py-8 flex  justify-between space-x-2 items-center"
+                onClick={() => toggleMenu(4)}
+              >
+                <span
+                  className={clsx(
+                    "text-base font-semibold peer transition-colors ease-in-out"
+                  )}
+                >
+                  How does Dropbox help to improve sales efficiency?
+                </span>
+                <div className="p-3">
+                  {openMenu === 4 ? (
+                    <ChevronUpIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  ) : (
+                    <ChevronDownIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  )}
+                </div>
+              </div>
+              <div
+                className={clsx(
+                  "overflow-hidden transition-all w-full duration-300 ease-in-out -translate-y-7",
+                  openMenu === 4
+                    ? "max-h-[500px] opacity-100"
+                    : "max-h-0 opacity-0"
+                )}
+              >
+                <div className="px-6  lg:w-9/12 w-full mb-6">
+                  An effective sales team needs to run seamlessly, like a
+                  well-oiled machine.
+                </div>
+                <ul
+                  style={{ scrollbarWidth: "none" }}
+                  className="lg:w-4/5 w-full box-border px-6 flex flex-col gap-y-4 overflow-scroll  list-disc   pl-12"
+                >
+                  <li>
+                    <p className="font-light inline">
+                      Create, store, and organize your pitch decks in your
+                      Dropbox cloud storage
+                    </p>
+                  </li>
+                  <li>
+                    <p className="font-light inline">
+                      Send pitches and proposals with DocSend
+                    </p>
+                  </li>
+                  <li>
+                    <p className="font-light inline">
+                      dd context to your proposals with screen recordings from
+                      Capture
+                    </p>
+                  </li>
+                  <li>
+                    <p className="font-light inline">
+                      When it’s time to sign new contracts or renewals, request
+                      eSignatures with Sign
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="flex flex-col relative border-b border-black hover:bg-[#eee9e2] border-opacity-40 items-start box-border">
+              <div
+                className="cursor-pointer w-full box-border px-6 py-8 flex  justify-between space-x-2 items-center"
+                onClick={() => toggleMenu(5)}
+              >
+                <span
+                  className={clsx(
+                    "text-base font-semibold peer transition-colors ease-in-out"
+                  )}
+                >
+                  How does Dropbox keep sales data secure?
+                </span>
+                <div className="p-3">
+                  {openMenu === 5 ? (
+                    <ChevronUpIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  ) : (
+                    <ChevronDownIcon
+                      className={clsx(
+                        "size-6 transition-colors ease-in-out text-black "
+                      )}
+                    />
+                  )}
+                </div>
+              </div>
+              <div
+                className={clsx(
+                  "overflow-hidden transition-all duration-300 ease-in-out -translate-y-7",
+                  openMenu === 5
+                    ? "max-h-[500px] opacity-100"
+                    : "max-h-0 opacity-0"
+                )}
+              >
+                <ul
+                  style={{ scrollbarWidth: "none" }}
+                  className="w-full box-border px-6 flex flex-col gap-y-6 overflow-scroll"
+                >
+                  <li className="w-full text-base font-light">
+                    Dropbox is built with multiple layers of protection to keep
+                    your team’s files secure.
+                  </li>
+                  <li className="w-full text-base font-light">
+                    Simplify data security for your sales team and enable them
+                    to build customer trust, with Dropbox tools that prioritize
+                    the protection of sensitive information.
                   </li>
                 </ul>
               </div>
--- app/ui/landing-page/edge.tsx
+++ app/ui/landing-page/edge.tsx
@@ -1,6 +1,9 @@
 import { ExpressV9 } from "../components/expression";
 import { collabsV2 } from "../navbar/links";
+import { golos } from "../fonts";
 import { SmallPicV3 } from "../components/why-rep";
+import { ActionB } from "../components/get-started";
+import Link from "next/link";
 import { ComplexFaq } from "../components/FAQ";
 export default function Edge() {
   return (
@@ -10,14 +13,54 @@ export default function Edge() {
           style={{ flexGrow: 1 }}
           className="xl:max-w-[1280px]  box-border flex flex-col items-center gap-y-20 w-full mx-auto py-20"
         >
-          <ExpressV9/>
+          <ExpressV9 />
           <div>
             <SmallPicV3 collabsV2={collabsV2} />
           </div>
-          <ComplexFaq/>
+          <ComplexFaq />
+        </div>
+        <div className="w-full bg-[#1e1919] flex justify-center  box-border py-20">
+          <div
+            id="way"
+            className=" lg:gap-y-0 gap-y-6 xl:max-w-[1280px]  justify-center   grid "
+          >
+            <h2
+              className={` text-2xl text-white   md:text-5xl px-4  lg:px-6 text-wrap ${golos.className}`}
+            >
+              Sales contracts as good as “Signed”
+            </h2>
+            <div className="grid gap-y-6 justify-start px-4 lg:justify-center box-border">
+              <ActionB
+                link="https://www.dropbox.com/try/teams"
+                text="Try for free"
+              />
+              <Link href="https://experience.dropbox.com/contact">
+                {" "}
+                <p className="inline-flex items-center text-base group text-white  underline underline-offset-2 decoration-1 hover:decoration-white transition-colors duration-200 decoration-stone-400">
+                  Contact Sales{" "}
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    className="ml-2 transition-transform duration-300 group-hover:translate-x-1"
+                    width="24"
+                    height="24"
+                    role="presentation"
+                    focusable="false"
+                  >
+                    <path
+                      d="M5 11.75h12m-5.25-6.5 6.25 6.5-6.25 6.5"
+                      stroke="currentColor"
+                      strokeWidth="1.5"
+                      strokeMiterlimit="10"
+                      vectorEffect="non-scaling-stroke"
+                    ></path>
+                  </svg>
+                </p>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
   );
 }
