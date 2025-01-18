230e8b59b00311a71c80e9daf43f795572a26e13
Hckerson <166761968+Hckerson@users.noreply.github.com>
2025-01-18 03:02:00 +0100

    create login and signup route functions

--- app/Auth/login.tsx
+++ app/Auth/login.tsx
@@ -1,7 +1,14 @@
-import { FormState } from "../lib/definitions";
-import { SignSchema } from "../lib/definitions";
-export async function Login(state: FormState, formData: FormData) {
-  const validatedFields = SignSchema.safeParse({
+'use server'
+import bcrypt from 'bcryptjs'
+import { State } from '../lib/definitions';
+import { LoginSchema } from '../lib/definitions';
+import { neon } from '@neondatabase/serverless';
+const sql = neon(`${process.env.DATABASE_URL}`)
+import 'dotenv/config'
+
+
+export async function login(prevState: State = {}, formData: FormData) {
+  const validatedFields = LoginSchema.safeParse({
     email: formData.get("email"),
     password: formData.get("password"),
   });
@@ -9,7 +16,33 @@ export async function Login(state: FormState, formData: FormData) {
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-	  message : 'Missing field' 
+      message: "",
     };
+  }
+
+  const {email, password} = validatedFields.data
+  try {
+    const response = await sql(`
+      SELECT * FROM users
+      WHERE email = $1`, [email])
+      console.log(response)
+    if (response.length === 0 ){
+      return {
+        message : 'User not found'
+      }
+    }
+    const storedPassword = response[0].password
+    const isValid = await bcrypt.compare(password, storedPassword)
+    console.log(isValid)
+    if (!isValid){
+      return {
+        message : 'Invalid Password'
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      message : 'Login error, Failed to validate user'
+    }
   }
 }
--- app/Auth/signUp.tsx
+++ app/Auth/signUp.tsx
@@ -1,15 +1,50 @@
-import { FormState } from "../lib/definitions";
-import { SignSchema } from "../lib/definitions";
-export async function SignUp(state: FormState, formData: FormData) {
-  const validatedFields = SignSchema.safeParse({
+"use server";
+import bcrypt from "bcryptjs";
+import { State } from "../lib/definitions";
+import { LoginSchema } from "../lib/definitions";
+import { neon } from "@neondatabase/serverless";
+const sql = neon(`${process.env.DATABASE_URL}`);
+import "dotenv/config";
+
+export async function signUp(state: State, formData: FormData) {
+
+  const validatedFields = LoginSchema.safeParse({
     email: formData.get("email"),
     password: formData.get("password"),
   });
-
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-      message : 'Missing field' 
+      message: "",
+    };
+  }
+  const { email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const response = await sql(
+      `
+    SELECT * FROM users 
+    WHERE email = $1`,
+      [email]
+    );
+
+    if (response.length == 0) {
+      const name = email.includes("@") ? email.split("@")[0] : "";
+      const result = await sql(
+        `
+        INSERT INTO users (name, email, password)
+        VALUES ($1, $2, $3)`,
+        [name, email, hashedPassword]
+      );
+    }
+    return {
+      message: "Email already exists, Login instead",
+    };
+
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Login error, Failed to create user",
     };
   }
 }
